5050e8676c613f91982d4a7d98397292d24227ca
Gad Josue Alva Castromonte <josuealva920@gmail.com>
2024-09-17 22:38:43 -0500

    agregando Controller y vistas

---
+++ src/main/java/pe/edu/cibertec/adopta_patitas/viewmodel/LoginModel.java
@@ -0,0 +1,6 @@
+package pe.edu.cibertec.adopta_patitas.viewmodel;
+
+public record LoginModel(String codigo,
+                         String mensaje,
+                         String nombreUsuario) {
+}
