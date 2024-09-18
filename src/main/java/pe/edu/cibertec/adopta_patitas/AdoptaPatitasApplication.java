e51060edcc0e3066ae16f5521e6e727e172556a3
Gad Josue Alva Castromonte <josuealva920@gmail.com>
2024-09-17 20:44:18 -0500

    carga inicial de patitas

---
+++ src/main/java/pe/edu/cibertec/adopta_patitas/AdoptaPatitasApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.adopta_patitas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdoptaPatitasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdoptaPatitasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/adopta_patitas/AdoptaPatitasApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.adopta_patitas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdoptaPatitasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
